815df1bcd0995f2698ac55995ce96997cf3bf06a
Emma M. Neil <emn3@williams.edu>
2019-03-03 17:07:48 -0500

    maybe a little closer

--- mergeSort.java
+++ mergeSort.java
@@ -2,29 +2,30 @@
 // Emma MR Neil (c) 2019
 
 class mergeSort{
-    int[]  data; // array of values to be sorted
-    int n; // pre: n<= data.length()
+    // pre: n<= data.length()
     // post: data is returned sorted in  ascending order
     
-    public int[] sort(data, n){
-	return sortHelper(data, n);
+    public int[] merge(int[] data){
+	//merges the arrays generated in sort
     }
 
-    public int[] sortHelper(data, n){
+    public int[] sort(int[] data, int[] temp, int low, int high){
 	// base case: if the list contains one element, it is already sorted
 	if(data.length() <2){
 	    return data;
 	}
-	int mid = n/2;
+	int n = high-low +1;
+	int mid = low + n/2;
+	
 	int[] temp = data[0,mid];
 	data = data[mid+1,n];
-	sortHelper(temp, mid);
-	sortHelper(data, mid);
-	
+	sort(temp, mid);
+	sort(data, mid);
+	merge(data);
 	
     public static void main(String[] args){
-	data = [2,3,1,5,3,6,7,13,11,1];
+	int[] list = [2,3,1,5,3,6,7,13,11,1];
 	System.out.print(data);
-	System.out.print(sort(data, 10);
+	System.out.print(sort(list,int[] empty,0,list.length-1 ));
     }
 }
